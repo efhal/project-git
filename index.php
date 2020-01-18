f38603b52ad8e165ba188d9fc8ef647cf92c70b4
efhal <chefhaldo@gmail.com>
2020-01-18 19:33:49 +0700

    sdf

--- index.php
+++ index.php
@@ -4,7 +4,28 @@
 <meta charset="ISO-8859-1">
 <title>HTML</title>
 </head>
+	<th>
+	<tr>
+		<td>No</td>
+		<td>Nama</td>
+		<td>Alamat</td>
+		<td>Asal</td>
+	</tr>
 <body>
+	<th>
+	<tr>
+		<td>No</td>
+		<td>Nama</td>
+		<td>Alamat</td>
+		<td>Asal</td>
+	</tr>
+	<th>
+	<tr>
+		<td>No</td>
+		<td>Nama</td>
+		<td>Alamat</td>
+		<td>Asal</td>
+	</tr>
 <table>
 	<th>
 	<tr>
