2d47d40386b24993ee740fba4713f7f71ba93dfa
efhal <chefhaldo@gmail.com>
2020-01-18 19:10:54 +0700

    Update tabel

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>HTML</title>
+</head>
+<body>
+
+
+
+<table>
+	<tr>
+		<td>No</td>
+		<td>Nama</td>
+		<td>Alamat</td>
+		<td>Asal</td>
+	</tr>
+</table>
+</body>
+</html>
