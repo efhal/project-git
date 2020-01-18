552e9b56e86d3477bfb7fe822f32339f4410c74c
efhal <chefhaldo@gmail.com>
2020-01-18 19:23:37 +0700

    UP

--- index.php
+++ index.php
@@ -5,12 +5,6 @@
 <title>HTML</title>
 </head>
 <body>
-<audio src=""></audio>
-<audio src=""></audio>
-<audio src=""></audio>
-<audio src=""></audio>
-<audio src=""></audio>
-<audio src=""></audio>
 
 <address>Ini adalah address</address>
 <h1>ini adalah contoh</h1>
