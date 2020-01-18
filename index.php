fe5782aaeb969f525a69035fee4a029efb9f0375
efhal <chefhaldo@gmail.com>
2020-01-18 19:58:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,25 +4,8 @@
 <meta charset="ISO-8859-1">
 <title>HTML</title>
 </head>
-<div>asdasd</div>
-<div>asdasd</div>
-<div>asdasd</div>
-  <table></table>
-    <table></table>
-<div>asdasd</div>
-asasas
-asasas
-
-sdfsdf
-
-sdfsdf
-asasas
-asasas
-<div>asdasd</div>
-
-<div>sjdhjksdhkjs</div>
-
-  <table></table>
-</body>
+<body>
+  <table>
+  </table>
   </body>
 </html>
